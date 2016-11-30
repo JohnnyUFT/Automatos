80b1df7782dda321bc218a6cdf86ca210c353dbb
JohnnyUFT <johnnyuft@gmail.com>
2016-11-30 10:37:54 -0300

    evolução do trabalho 3, os outros já estão prontos.

--- Trabalho 3/Trabalho3_1.py
+++ Trabalho 3/Trabalho3_1.py
@@ -18,18 +18,144 @@ from PIL import ImageTk, Image
 from tkinter import PhotoImage
 import sys # desnecessário, talvez?
 from tkinter import messagebox
+from tkinter import ttk
 
 def obterAlfbabeto():
-    alfabeto = ent1.get()
-    print(alfabeto)
+    alfabeto =   ent1.get()
+    q0(alfabeto)# começa a verificar o formalismo para o conjunto dado
+    qtdEstados = int(ent2.get())
+    # de acordo com qtsEstados, criar a lista para escolha do(s) estado(s) final(s):
+    preencheListaEstados(qtdEstados)
+    estInicial = ent3.get()
+    estFinal =   cbb1.get()# corrigir
+    print(alfabeto,qtdEstados,estInicial,estFinal)
 
 def obterPalavra():
     palavra = ent5.get()
     print(palavra)
     analisaPalavra(palavra)
 
+def q0(alfabeto):
+    """
+    Implementa toda lógica necessária ao controle do estado inicial 'q0'.
+
+    O primeiro símbolo do alfabeto deve ser, necessariamente
+    um 'abre-chaves', do contrário, a entrada já é considerada errada.
+    :param alfabeto: str
+    :return: void
+    """
+    i = 0  # marca índice da string (cadeia de simbolos da fita)
+    print("i em q0: %i, alfabeto %s, simbolo %s" % (i, alfabeto, alfabeto[i]))
+    if ((alfabeto[i] == '{') and len(alfabeto) > 1):
+        q1(alfabeto, i + 1)
+    else:
+        qErro()
+
+
+def q1(alfabeto, i):
+    """
+    Implementação do estado 'q1'.
+
+    Se o alfabeto informado estiver correto,
+    o símbolo informado aqui, deverá ser exclusivamente alfanumérico.
+    :param alfabeto: str
+    :param i: int
+    :return: void
+    """
+    print("i em q1: %i, alfabeto %s, simbolo %s" % (i, alfabeto, alfabeto[i]))
+    if i <= len(alfabeto)-2:
+        if verAlfaNumerico(alfabeto, i):
+            q2(alfabeto, i+1)
+        else:
+            qErro()
+    else:
+        qErro()
+
+
+def q2(alfabeto, i):
+    """
+    Implementação das regras definidas para estado 'q2'.
+
+    Se o alfabeto informado estiver correto, o símbolo processado aqui
+    deverá ser uma vírgula.
+    :param alfabeto: str
+    :param i: int
+    :return: void
+    """
+    print("i em q2: %i, alfabeto %s, simbolo %s" % (i, alfabeto, alfabeto[i]))
+    if i <= len(alfabeto)-1:
+        if alfabeto[i] == ',' and i < len(alfabeto)-1:
+            q1(alfabeto, i + 1)
+        elif alfabeto[i] == '}' and i == len(alfabeto)-1: # não há mais simbolos na 'fita'
+            q3(alfabeto, i) # chegamos ao último símbolo e este é uma vírgula
+        else:
+            qErro()
+    else:
+        qErro()
+
+
+def q3(alfabeto, i):
+    """
+    Implementação das regras definidas para estado 'q3'.
+
+    Este é o estado de aceitação. É nele que está contido o teste
+    para confirmar se o alfabeto informado é aceito ou rejeitado.
+    :param alfabeto: str
+    :param i: int
+    :return: void
+    """
+    print("i em q3: %i, alfabeto %s, simbolo %s" % (i, alfabeto, alfabeto[i]))
+    if i == len(alfabeto)-1:
+        messagebox.showinfo("Informação", "Alfabeto aceito!")
+        # chamar o que agora?
+        # tem que chamar algo aqui
+        # para continuar o programa
+    else:
+        qErro()
+
+
+def qErro():
+    """
+    Estado de erro.
+
+    Não faz nada a não ser, mostrar esta mensagem na tela e interromper
+    o processo de transições entre os estados.
+    :return: void
+    """
+    messagebox.showerror("Erro", "Alfabeto rejeitado.")
+
+def verAlfaNumerico(alfabeto, i):
+    """
+    Verifica se o símbolo recebido é alfanumérico.
+
+    Este método testa se o símbolo está entre A,...,Z
+                                           ou a,...,z
+                                           ou 0,...,9
+    :param alfabeto: str
+    :param i: int
+    :return: boolean
+    """
+    #print("Em verAlphaNumeric: %s, %d "%(alfabeto, i))
+    if (ord(alfabeto[i]) >= 65 and ord(alfabeto[i]) <= 90) \
+            or (ord(alfabeto[i]) >= 97 and ord(alfabeto[i]) <= 122) \
+            or (ord(alfabeto[i]) >= 48 and ord(alfabeto[i]) <= 57):
+        return True
+    return False
+
 def analisaPalavra(palavra):
-    messagebox.showinfo("Informação", "Palavra aceita!")
+    """
+    Realiza última verificação para atestar se a palavra foi aceita, ou não.
+    :param palavra:
+    :return: void
+    """
+    messagebox.showinfo("Parabéns", "Palavra foi aceita!")
+
+def preencheListaEstados(qtdEstados):
+    for i in range(qtdEstados):
+        estado = 'q'+str(i)
+        print(estado, type(estado))
+        cbb1['values'] = estado
+
 
 #  AQUI INICIA A IMPLEMENTAÇÃO DA PARTE GRÁFICA
 Janela = tk.Tk()  # instancia da Janela Principal (mainWindow)
@@ -37,6 +163,9 @@ Janela = tk.Tk()  # instancia da Janela Principal (mainWindow)
 Posteriomente, separar este método em uma classe separada
 e trabalhar com implementação OO e GUI.
 """
+
+values = StringVar()
+
 #  principais frames (conteiners) da tela principal
 frame1 = Frame(Janela, height=100, bg="white")
 frame2 = Frame(Janela, height=100, bg="")
@@ -68,13 +197,17 @@ lb11.grid(row=0, column=0, stick=W)
 #  elementos contidos no frame2
 lb2 = Label(frame2, text="Alfabeto: ", font=fonte1)
 ent1 = Entry(frame2, text="", font=fonte1)
-bt1 = Button(frame2, text="Ok ", font=fonte1, command=obterAlfbabeto)
 lb3 = Label(frame2, text="Qtd estados: ", font=fonte1)
 ent2 = Entry(frame2, text="", font=fonte1)
+bt1 = Button(frame2, text="Ok", font=fonte1)
 lb4 = Label(frame2, text="Estado Inicial: ", font=fonte1)
-ent3 = Entry(frame2, text="", font=fonte1)# substituir por ComboBox
+ent3 = Entry(frame2, text="0", font=fonte1)
+ent3.insert(0, 'q0')
 lb5 = Label(frame2, text="Estado Final: ", font=fonte1)
-ent4 = Entry(frame2, text="", font=fonte1)# substituir por ComboBox
+cbb1 = ttk.Combobox(frame2, textvariable=values, state='readonly')# adicionar propriedades?
+values = ['']# é preciso preencher isso aqui
+
+bt2 = Button(frame2, text="Ok ", font=fonte1, command=obterAlfbabeto)
 
 # elementos contidos no frame3
 lb6 = Label(frame3, text="Função de Transição: ", font=fonte1)
@@ -90,18 +223,19 @@ lb9 = Label(frame4, image=photo, bg="white")
 
 # elementos contidos no frame5
 # refazer isso aqui
-lb10 = Label(frame5, text="Palavra Rejeitada!") # mudar o tipo do widget?
+lb10 = Label(frame5, text="Palavra Rejeitada!") # setar o texto conforme resultado da análise
 
 #  disposição dos widgets do frame2
 lb2.grid(row=0, column=0, stick=W)
 ent1.grid(row=1, column=0, stick=W)
-bt1.grid(row=1, column=1, stick=W)
-lb3.grid(row=0, column=2, stick=W)
-ent2.grid(row=1, column=2, stick=W)
+lb3.grid(row=0, column=1, stick=W)
+ent2.grid(row=1, column=1, stick=W)
+bt1.grid(row=1, column=2, stick=W)
 lb4.grid(row=0, column=3, stick=W)
 ent3.grid(row=1, column=3, stick=W)
 lb5.grid(row=0, column=4, stick=W)
-ent4.grid(row=1, column=4, stick=W)
+cbb1.grid(row=1, column=4, stick=W)
+bt1.grid(row=1, column=5, stick=W)
 
 # disposição dos widgets do frame3
 lb6.grid(row=0, column=0, stick=W)
@@ -119,5 +253,5 @@ lb10.grid(row=0, column=0, stick=E+W)# apenas para teste, remover depois
 
 Janela.title("Linguagens Formais & Autômatos - Trabalho #3")
 #  Largura x Altura + Esquerda + Topo
-Janela.geometry("700x550+300+100")
+Janela.geometry("750x550+300+100")
 Janela.mainloop()
